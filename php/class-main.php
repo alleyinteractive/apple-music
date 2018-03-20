750188225b5793b561d9b4069c56296e92b11b19
Grant Mangham <grant@alleyinteractive.com>
2018-03-20 10:42:01 -0700

    add basic settings

--- php/class-main.php
+++ php/class-main.php
@@ -14,7 +14,7 @@ class Main {
 	/**
 	 * @var string $base_url Base Apple Music API endpoint URL.
 	 */
-	protected $base_url = 'https://api.music.apple.com/v1/catalog';
+	protected $base_url = 'https://api.music.apple.com/v1';
 
 	/**
 	 * @var string $storefront Apple Music Storefront to query.
@@ -32,38 +32,65 @@ class Main {
 	public function setup() {
 		$this->storefront = apply_filters( 'apple_music_storefront', 'us' );
 
-		$this->token = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldSWDQ2U1A5TjQifQ.eyJpc3MiOiJBSEtFSzNUMzZQIiwiaWF0IjoxNTE2NjYxOTY2LCJleHAiOjE1MzIzODMxNjZ9.9cCIFu1fq0wJV49HwbVdpreVQ2KQf14Yz0PRD3IjFGfayFXipsv8maSfAZLPuRNLFyhZWY8V2FB7uVBdYQOMNw';
+		$settings = new Settings();
+		$token    = $settings->get_token();
+
 	}
 
 	/**
 	 * Search for a requested term.
 	 *
 	 * @param string $term
+	 *
 	 * @return object|null
 	 */
 	public function search( $term ) {
-		return $this->send_request( 'GET', 'search', compact( 'term' ) );
+
+		$url = sprintf( '%s/%s/%s/%s',
+			$this->base_url,
+			'catalog',
+			$this->storefront,
+			'search'
+		);
+
+		return $this->send_request( 'GET', $url, compact( 'term' ) );
 	}
 
 	/**
-	 * Send an API request.
-	 *
-	 * @param string $method GET or POST
-	 * @param string $endpoint
-	 * @param array $params
-	 * @return object|null
+	 * Fetch and cache storefronts.
+	 * 
+	 * @return mixed|null
 	 */
-	protected function send_request( $method, $endpoint, $params = array() ) {
-		if ( empty( $this->token ) ) {
-			return null;
+	public function get_storefronts() {
+		$transient   = 'apple-music-storefronts';
+		$storefronts = get_transient( $transient );
+		if ( false === $storefronts ) {
+
+			$url = sprintf( '%s/%s',
+				$this->base_url,
+				'storefronts'
+			);
+
+			$storefronts = $this->send_request( 'GET', $url );
+
+			if ( ! empty( $storefronts ) ) {
+				set_transient( $transient, $storefronts, DAY_IN_SECONDS );
+			}
 		}
 
-		// Build the request URL
-		$url = sprintf( '%s/%s/%s',
-			$this->base_url,
-			$this->storefront,
-			$endpoint
-		);
+		return $storefronts;
+	}
+
+	/**
+	 * Make API request.
+	 * 
+	 * @param $method
+	 * @param $url
+	 * @param array $params
+	 *
+	 * @return mixed|null
+	 */
+	protected function send_request( $method, $url, $params = array() ) {
 
 		$url_safe = esc_url_raw( add_query_arg( $params, $url ) );
 
@@ -72,22 +99,24 @@ class Main {
 				$url_safe,
 				5, // request timeout in seconds
 				900, // cache timeout in seconds
-				array( 'http_api_args' => array(
-					'headers' => array(
-						'Authorization' => "Bearer {$this->token}",
+				array(
+					'http_api_args' => array(
+						'headers' => array(
+							'Authorization' => "Bearer {$this->token}",
+						)
 					)
-				)
-			) );
+				) );
 		} else {
 			$response = wp_safe_remote_request(
 				$url_safe,
 				array(
-					'method' => $method,
+					'method'  => $method,
 					'headers' => array(
 						'Authorization' => "Bearer {$this->token}",
 					)
 				)
 			);
+
 
 			if ( ! empty( $response ) && ! is_wp_error( $response ) ) {
 				$response = wp_remote_retrieve_body( $response );
@@ -99,8 +128,8 @@ class Main {
 		}
 
 		// Return the results of the API request
-		$response = json_decode( $response );
-
+		$response = json_decode( $response ); // wont work for errors
+		
 		if ( ! empty( $response->results ) ) {
 			return $response->results;
 		}
---
+++ php/class-settings.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+ * Settings for the Apple Music embed tool.
+ *
+ * @package Apple_Music
+ */
+
+namespace Apple_Music;
+
+class Settings {
+
+	use Util\Singleton;
+
+	/**
+	 * Set up the singleton.
+	 */
+	public function setup() {
+		add_action( 'admin_menu', [ $this, 'options_page' ] );
+		add_action( 'admin_init', [ $this, 'register_settings' ] );
+	}
+
+
+	/**
+	 * Register our settings.
+	 */
+	function register_settings() {
+
+		register_setting( 'apple_music', 'apple_music_options' );
+
+		add_settings_section(
+			'apple_music_settings',
+			esc_html__( 'Settings', 'apple-music' ),
+			[ $this, 'section' ],
+			'apple_music'
+		);
+
+		add_settings_field(
+			'apple_music_token',
+			esc_html__( 'Apple Music Token', 'apple-music' ),
+			[ $this, 'add_token_field' ],
+			'apple_music',
+			'apple_music_settings',
+			[
+				'label_for' => 'token',
+			]
+		);
+
+		add_settings_field(
+			'apple_music_storefront',
+			esc_html__( 'Apple Music Storefront', 'apple-music' ),
+			[ $this, 'add_storefront_field' ],
+			'apple_music',
+			'apple_music_settings',
+			[
+				'label_for' => 'storefront',
+			]
+		);
+	}
+
+	/**
+	 * Section description.
+	 *
+	 * @param $args
+	 */
+	function section( $args ) {
+		?>
+		<p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Section description placeholder', 'apple-music' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * Helper to get token.
+	 *
+	 * @return string
+	 */
+	function get_token() {
+		$options = get_option( 'apple_music_options' );
+		$token   = ! empty( $options['token'] ) ? $options['token'] : '';
+
+		return $token;
+	}
+
+	/**
+	 * Helper to get storefront.
+	 *
+	 * @return string
+	 */
+	function get_storefront() {
+		$options    = get_option( 'apple_music_options' );
+		$storefront = ! empty( $options['storefront'] ) ? $options['storefront'] : '';
+
+		return $storefront;
+	}
+
+	/**
+	 * Token field.
+	 *
+	 * @param $args
+	 */
+	function add_token_field( $args ) {
+		?>
+		<input type="text" name="apple_music_options[token]" value="<?php echo esc_html( $this->get_token() ); ?>" class="large-text"/>
+
+		<p class="description">
+			<?php esc_html_e( 'Field description placeholder', 'apple-music' ); ?>
+		</p>
+
+		<?php
+	}
+
+	/**
+	 * Storefront field.
+	 *
+	 * @param $args
+	 */
+	function add_storefront_field( $args ) {
+		$options     = get_option( 'apple_music_options' );
+		$storefront  = ! empty( $options['storefront'] ) ? $options['storefront'] : 'us';
+		$api         = new Main();
+		$storefronts = $api->get_storefronts();
+		?>
+		<select id="<?php echo esc_attr( $args['label_for'] ); ?>" name="apple_music_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
+			<?php foreach ( $storefronts->data as $sf ) : ?>
+				<option value="<?php echo $sf->id; ?>" <?php selected( $storefront, $sf->id ); ?>>
+					<?php esc_html_e( $sf->attributes->name ); ?>
+				</option>
+			<?php endforeach; ?>
+		</select>
+		<p class="description">
+			<?php esc_html_e( 'Field description placeholder', 'apple-music' ); ?>
+		</p>
+		<?php
+	}
+
+	/**
+	 * Add settings to admin menu.
+	 */
+	function options_page() {
+		add_options_page(
+			esc_html__( 'Apple Music', 'apple-music' ),
+			esc_html__( 'Apple Music', 'apple-music' ),
+			'manage_options',
+			'apple-music',
+			[ $this, 'options_page_html' ]
+		);
+	}
+
+	/**
+	 * Body of settings page.
+	 */
+	function options_page_html() {
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+		?>
+
+		<div class="wrap">
+			<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+			<form action="options.php" method="post">
+				<?php
+				settings_fields( 'apple_music' );
+				do_settings_sections( 'apple_music' );
+				submit_button( 'Save Settings' );
+				?>
+			</form>
+		</div>
+		<?php
+	}
+
+}
