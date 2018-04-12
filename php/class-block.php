f01a58160dd53639033de542b393a4e005439631
Steven Slack <stevenslack@gmail.com>
2018-04-12 13:25:38 -0400

    Add comments to class-block.php

--- php/class-block.php
+++ php/class-block.php
@@ -5,22 +5,26 @@ namespace Apple_Music;
 class Block {
 
 	public function __construct() {
-		add_action( 'enqueue_block_editor_assets', [ $this, 'apple_music_enqueue_block_editor_assets' ] );
-		add_action( 'enqueue_block_assets', [ $this, 'apple_music_enqueue_block_assets' ] );
-
+		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_assets' ] );
+		add_action( 'enqueue_block_assets', [ $this, 'enqueue_block_assets' ] );
 	}
 
-	function apple_music_enqueue_block_editor_assets() {
+	/**
+	 * Enqueue block editor only JavaScript and CSS
+	 */
+	public function enqueue_block_editor_assets() {
 		wp_enqueue_script(
 			'apple-music-block',
 			PLUGIN_DIR_URL . 'assets/js/blocks.bundle.min.js',
-			[ 'wp-blocks', 'wp-i18n', 'wp-element', 'underscore' ],
+			[ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ],
 			APPLE_MUSIC_VERSION
 		);
 	}
 
-
-	function apple_music_enqueue_block_assets() {
+	/**
+	 * Enqueue front end and editor JavaScript and CSS
+	 */
+	public function enqueue_block_assets() {
 		wp_enqueue_style(
 			'apple-music-block',
 			PLUGIN_DIR_URL . 'assets/css/block.css',
