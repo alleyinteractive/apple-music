90ba88ae7272e4758f7e4fc9bea2a456b34df392
Steven Slack <stevenslack@gmail.com>
2018-04-13 17:07:33 -0400

    panel for selecting music and music type

--- blocks/apple-music/index.js
+++ blocks/apple-music/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import musicTypes from './musicTypes';
 
 const { __ } = window.wp.i18n;
-const { registerBlockType } = window.wp.blocks;
+const { registerBlockType, InspectorControls } = window.wp.blocks;
+// Get components from Components
+const {
+  PanelBody,
+  PanelRow,
+  TextControl,
+  SelectControl,
+} = window.wp.components;
 
 /**
  * Register block
@@ -12,7 +20,7 @@ export default registerBlockType(
     title: __('Apple Music', 'apple-music'),
     description:
       __('Embed the apple music player into a post.', 'apple-music'),
-    category: 'embed',
+    category: 'widgets',
     icon: 'format-audio',
     keywords: [
       __('Apple Music', 'apple-music'),
@@ -23,19 +31,70 @@ export default registerBlockType(
       html: false,
     },
     attributes: {
-      message: {
+      music: {
         type: 'array',
         source: 'children',
-        selector: '.message-body',
+        selector: '.display-music',
+      },
+      musicType: {
+        type: 'string',
+        default: 'artists',
       },
     },
-    edit: () => (
-      <div>
-        <h2>{__('Call to Action', 'apple-music')}</h2>
+    edit: ({
+      attributes,
+      setAttributes,
+      isSelected,
+      className,
+    }) => {
+      const onChangeMessage = (music) => {
+        setAttributes({ music });
+      };
+      // on Select assign the musicType attribute.
+      const onSelect = (musicType) => {
+        setAttributes({ musicType });
+      };
+
+      // Get the current selected musicType object
+      const typeObject = musicTypes.find((type) => (
+        type.value === attributes.musicType
+      ));
+
+      return (
+        <div>
+          { isSelected &&
+            <InspectorControls key="inspector">
+              <PanelBody title={__('Apple Music Settings', 'apple-music')}>
+                <TextControl
+                  label={`Search ${typeObject.label}`}
+                  value={attributes.music}
+                  onChange={onChangeMessage}
+                />
+                <PanelRow>
+                  <SelectControl
+                    label={__('Music Type', 'apple-music')}
+                    value={attributes.musicType}
+                    options={musicTypes.map(({ value, label }) => (
+                      { value, label }
+                    ))}
+                    onChange={onSelect}
+                  />
+                </PanelRow>
+              </PanelBody>
+            </InspectorControls>
+          }
+          <div className={className}>
+            {attributes.music}
+          </div>
+        </div>
+      );
+    },
+    save: (props) => (
+      <div className={props.className}>
+        <div className="display-music">
+          {props.attributes.music}
+        </div>
       </div>
-    ),
-    save: () => (
-      window.wp.element.createElement('hr')
     ),
   },
 );
---
+++ blocks/apple-music/musicTypes.js
@@ -0,0 +1,36 @@
+// Import internationalization function.
+const { __ } = window.wp.i18n;
+/**
+ * The list of the types of resources to include in the results. The possible values are activities, artists, apple-curators, albums, curators, songs, playlists, music-videos, and stations.
+ *
+ * @see  https://developer.apple.com/library/content/documentation/NetworkingInternetWeb/Conceptual/AppleMusicWebServicesReference/Searchforresources.html#//apple_ref/doc/uid/TP40017625-CH58-SW1
+ * @type {Array} music type values and labels
+ */
+const musicTypes = [
+  {
+    value: 'artists', label: __('Artists', 'apple-music'),
+  },
+  {
+    value: 'songs', label: __('Songs', 'apple-music'),
+  },
+  {
+    value: 'albums', label: __('Albums', 'apple-music'),
+  },
+  {
+    value: 'playlists', label: __('Playlists', 'apple-music'),
+  },
+  {
+    value: 'activities', label: __('Activities', 'apple-music'),
+  },
+  {
+    value: 'curators', label: __('Curators', 'apple-music'),
+  },
+  {
+    value: 'radio', label: __('Radio', 'apple-music'),
+  },
+  {
+    value: 'music-videos', label: __('Music Videos', 'apple-music'),
+  },
+];
+
+export default musicTypes;
