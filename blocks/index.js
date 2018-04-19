967c6a01b78521e689a48c0e43fd146889a43dd1
Steven Slack <stevenslack@gmail.com>
2018-04-19 15:45:44 -0400

    Register gutenberg apple music block

--- blocks/index.js
+++ blocks/index.js
@@ -1,3 +1,55 @@
+const { __ } = window.wp.i18n;
+const { registerBlockType } = window.wp.blocks;
+
 /**
- * Apple Music Gutenburg Block
+ * Register Apple Music block
  */
+export default registerBlockType(
+  'apple-music/widget',
+  {
+    title: __('Apple Music', 'apple-music'),
+    description:
+      __('Embed the apple music player into a post.', 'apple-music'),
+    category: 'widgets',
+    icon: 'format-audio',
+    keywords: [
+      __('Apple Music', 'apple-music'),
+    ],
+    supports: {
+      html: false,
+    },
+    attributes: {
+      query: {
+        type: 'array',
+        source: 'children',
+        selector: '.display-music',
+      },
+      musicType: {
+        type: 'string',
+        default: 'artists',
+      },
+      musicID: {
+        type: 'integer',
+      },
+      item: {
+        type: 'object',
+      },
+      height: {
+        type: 'string',
+      },
+      width: {
+        type: 'string',
+        default: '100%',
+      },
+      embedType: {
+        type: 'string',
+        default: 'preview-player',
+      },
+      iframeSrc: {
+        type: 'string',
+      },
+    },
+    edit: '',
+    save: '',
+  },
+);
