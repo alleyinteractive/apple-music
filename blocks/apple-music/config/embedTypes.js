d7f67ef71c53f1719b1994a7cde82896dbc7afc0
Steven Slack <stevenslack@gmail.com>
2018-04-25 08:53:22 -0400

    add color configurations to embedType config

--- blocks/apple-music/config/embedTypes.js
+++ blocks/apple-music/config/embedTypes.js
@@ -17,10 +17,42 @@ const embedTypes = [
   {
     value: 'text-lockup',
     label: __('Text Lockup', 'apple-music'),
+    styles: [
+      {
+        value: 'standard-black',
+        label: __('Standard Black', 'apple-news'),
+      },
+      {
+        value: 'standard-white',
+        label: __('Standard White', 'apple-news'),
+      },
+      {
+        value: 'mono-white',
+        label: __('Mono White', 'apple-news'),
+      },
+      {
+        value: 'mono-black',
+        label: __('Mono Black', 'apple-news'),
+      },
+    ],
   },
   {
     value: 'app-icon',
     label: __('App Icon', 'apple-music'),
+    styles: [
+      {
+        label: __('Standard', 'apple-news'),
+        hex: '',
+      },
+      {
+        label: __('White', 'apple-news'),
+        hex: 'FFFFFF',
+      },
+      {
+        label: __('Black', 'apple-news'),
+        hex: '000000',
+      },
+    ],
   },
 ];
 
