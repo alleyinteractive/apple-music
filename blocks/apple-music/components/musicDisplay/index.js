edeb49f9116b55d8571800ac62bd8675f1d6d851
Steven Slack <stevenslack@gmail.com>
2018-04-27 14:34:54 -0400

    use strings to display raw HTML

--- blocks/apple-music/components/musicDisplay/index.js
+++ blocks/apple-music/components/musicDisplay/index.js
@@ -1,45 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PreviewPlayer from 'Components/previewPlayer';
+import { getNestedObject } from 'Utils';
+
+const { RawHTML } = window.wp.element;
 
 const MusicDisplay = ({
   attributes: {
     embedType,
     height,
     iframeSrc,
+    item,
     width,
   },
   className,
-}) => (
-  <div className={className}>
-    {
-      'preview-player' === embedType &&
-      <PreviewPlayer
-        height={height}
-        iframeSrc={iframeSrc}
-        width={width}
-      />
-    }
-    {
-      'badge' === embedType &&
-      <div>
-        {'THIS IS WHERE THE BADGE WILL GO'}
-      </div>
-    }
-    {
-      'text-lockup' === embedType &&
-      <div>
-        {'THIS IS WHERE THE TEXT LOCKUP WILL GO'}
-      </div>
-    }
-    {
-      'app-icon' === embedType &&
-      <div>
-        {'THIS IS WHERE THE APP ICON WILL GO'}
-      </div>
-    }
-  </div>
-);
+}) => {
+  const URL = getNestedObject(item, ['attributes', 'url']);
+  const badge = `<a href=${URL} style="display:inline-block;overflow:hidden;background:url(https://tools.applemusic.com/assets/shared/badges/en-us/music-lrg.svg) no-repeat;width:157px;height:45px;"></a>`;
+
+  return (
+    <div className={className}>
+      {
+        'preview-player' === embedType &&
+        <PreviewPlayer
+          height={height}
+          iframeSrc={iframeSrc}
+          width={width}
+        />
+      }
+      {
+        'badge' === embedType &&
+        <div>
+          <RawHTML>
+            {badge}
+          </RawHTML>
+        </div>
+      }
+      {
+        'text-lockup' === embedType &&
+        <div>
+          {'THIS IS WHERE THE TEXT LOCKUP WILL GO'}
+        </div>
+      }
+      {
+        'app-icon' === embedType &&
+        <div>
+          {'THIS IS WHERE THE APP ICON WILL GO'}
+        </div>
+      }
+    </div>
+  );
+};
 
 MusicDisplay.propTypes = {
   attributes: PropTypes.shape({
