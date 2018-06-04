2e0c656f704f788bf5c8ed5cbd3309ef9e6d2ea2
Steven Slack <stevenslack@gmail.com>
2018-06-04 12:56:45 -0400

    Update media modal iframe to accommodate new embed URL

--- php/class-shortcode.php
+++ php/class-shortcode.php
@@ -104,19 +104,18 @@ class Shortcode {
 
 		// If necessary, set a default format.
 		$format = array_key_exists( $shortcode_atts['format'], $formats ) ? $shortcode_atts['format'] : $types[ $shortcode_atts['type'] ]['default_format'];
-
+		
 		// Embeds only (album, song, playlist).
 		if ( array_key_exists( $shortcode_atts['type'], $player_types ) && 'player' === $format ) {
-			$url = sprintf( '%1$s/%2$s/%3$s/%4$s/%5$s%6$s',
+			$url = sprintf( '%1$s/%2$s/%3$s/%4$s%5$s',
 				'https://embed.music.apple.com', // 1
 				$storefront, // 2
 				$player_types[ $shortcode_atts['type'] ]['singular'], // 3
-				sanitize_title( $shortcode_atts['name'] ), // 4
-				$shortcode_atts['id'], // 5
-				! empty( $affiliate_token ) ? '?at=' . $affiliate_token : '' // 6
+				$shortcode_atts['id'], // 4
+				! empty( $affiliate_token ) ? '?at=' . $affiliate_token : '' // 5
 			);
 
-			$output = sprintf( '<iframe allow="encrypted-media" style="padding:0;width:%1$s;height:%2$s;max-width:100%;border:none;overflow:hidden;background:transparent;" src="%3$s"></iframe>',
+			$output = sprintf( '<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="%2$s" width="%1$s" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" style="padding:0;width:%1$s;height:%2$s;max-width:100%%;border:none;overflow:hidden;background:transparent;" src="%3$s"></iframe>',
 				$player_types[ $shortcode_atts['type'] ]['default_width'], // 1
 				$player_types[ $shortcode_atts['type'] ]['default_height'], // 2
 				esc_url( $url ) // 3
