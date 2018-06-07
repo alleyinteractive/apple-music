1f54250960269d8ecc2e50897c6445aecbc9c20c
Steven Slack <stevenslack@gmail.com>
2018-06-07 10:03:45 -0400

    version bump

--- apple-music.php
+++ apple-music.php
@@ -3,7 +3,7 @@
  * Plugin Name: Apple Music
  * Plugin URI: https://github.com/alleyinteractive/apple-music
  * Description: Search Apple Music and embed content in your site.
- * Version: 0.0.1
+ * Version: 0.1.1
  * Author: Alley Interactive
  * Author URI: http://www.alleyinteractive.com/
  * Text Domain: apple-music
@@ -15,7 +15,7 @@ namespace Apple_Music;
 define( __NAMESPACE__ . '\PATH', __DIR__ );
 
 define( 'PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
-define( 'APPLE_MUSIC_VERSION', '0.1.0' );
+define( 'APPLE_MUSIC_VERSION', '0.1.1' );
 
 require_once PATH . '/php/autoload.php';
 require_once PATH . '/php/includes/includes.php';
